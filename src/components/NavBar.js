0b5d74f0f2b44106696a2fcebc008377f55d81b8
Jeremias85 <jeremias85@hotmail.com>
2023-12-11 23:22:40 -0300

    PreEntrega2+Arauz

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ function Navbar(props) {
         <div>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <NavLink className="navbar-brand" to={'/'}>Vintage Sur</NavLink>
+                    <NavLink className="navbar-brand" to={'/'}>Vintage Sur </NavLink>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
